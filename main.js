db278f453a28ecb4005da83b4156c217894b4123
Sxnty <sxntycsgo@gmail.com>
2022-12-12 16:11:30 -0300

    added new things

--- main.js
+++ main.js
@@ -1,27 +1,67 @@
-mostrarMenu()
-
-function mostrarMenu() {
-  let opcion; /* declaro las variables afuera del do debido al scope del mismo */
-  let menu;
+const menuAdmin = () => {
+  const AdminLogin = {
+    //declaro las credenciales de logeo de administradores
+    user: "Admin",
+    password: "Admin",
+  };
+  let user = prompt("Ingrese su Usuario");
+  let password = prompt("Ingrese su Contraseña");
+  if (user == AdminLogin.user && password == AdminLogin.password) {
+    mostrarMenu();
+  }
+};
+const mostrarMenu = () => {
+  let menuOption = prompt(
+    "1)Crear prestamo 2)Historial de prestamos 3)Cerrar sesion"
+  );
+  let option = parseInt(menuOption);
+  switch (option) {
+    case 1:
+      crearPrestamo();
+      break;
+    case 2:
+      historial();
+      break;
+    case 3:
+      return;
+      break;
+    default:
+      alert("Debes ingresar una opcion correcta.");
+  }
+};
+const crearPrestamo = () => {
+  function Prestamo(nombre, cantidad, cuotas, mensual) {
+    this.nombre = nombre;
+    this.cantidad = cantidad;
+    this.mensual = mensual;
+    this.cuotas = cuotas;
+  }
+  let opcionsalir;
   do {
-    menu = prompt(
-      "¿Que desea realizar? 1) Ingresar como Administrador 2)Ingresar como cliente 3)Finalizar Programa"
+    let nombre = prompt("Ingrese el nombre del cliente.");
+    do {
+      let cantidad = prompt("Ingrese la cantidad del prestamo solicitado.");
+      Cantidad = parseInt(cantidad);
+      if (isNaN(Cantidad)) {
+        alert("Debe ingresar un numero");
+      }
+    } while (isNaN(Cantidad));
+    do {
+      let cuotas = prompt("Ingrese la cantidad de cuotas solicitadas.");
+      Cuotas = parseInt(cuotas);
+      if (isNaN(Cuotas)) {
+        alert("Debe ingresar un numero");
+      }
+    } while (isNaN(Cuotas));
+    let mensual = Cantidad / Cuotas;
+    const prestamo1 = new Prestamo(nombre, Cantidad, Cuotas, mensual);
+    alert(
+      `El prestamo se cargo correctamente a nombre de ${prestamo1.nombre} con una cantidad total de $${prestamo1.cantidad} y un debito mensual de $${prestamo1.mensual} durante ${prestamo1.cuotas} meses`
     );
-    opcion = parseInt(menu);
-
-    switch (opcion) {
-      case 1:
-        calcularCuotas();
-        break;
-      case 2:
-        
-        break;
-      case 3:
-        alert("Usted finalizo el programa.");
-        break;
-      default:
-        alert("Debe ingresar una opcion correcta.");
-        break;
-    }
-  } while (menu != "3");
-}
+    opcionsalir = prompt(
+      "Si desea salir ingrese 1 en caso contrario ingrese cualquier numero."
+    );
+  } while (opcionsalir != "1");
+  mostrarMenu()
+};
+menuAdmin();
